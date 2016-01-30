7d69ac209f5beab052862d3078b99fca95fdd141
Axel Schumacher <contact@axelschumacher.fr>
2016-01-30 15:51:53 -0800

    WIP:  missing

---
+++ missing.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef uint8_t u;
+
+u missing( u *list, int n, int b )
+{
+    u mask = 1 << b;
+    u  = 0;
+    for( int i = 0; i < n; i++ )
+    {
+        u next = list[i] & mask;
+        if( 
+    }
+}
